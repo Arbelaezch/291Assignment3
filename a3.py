d5dc0d23ea82a99f6635688f81cbdb6ccc0ccc2e
Arbelaezch <arbelaezch@gmail.com>
2019-10-24 11:53:46 -0600

    Update a3.py

--- a3.py
+++ a3.py
@@ -22,27 +22,83 @@ def login(uid,pwd):
 	global connection, cursor
 
 	user = (uid,pwd)
-	cursor.execute('SELECT * FROM members WHERE uid=? AND pwd=?;', user)
+	cursor.execute('SELECT utype FROM users WHERE uid=? AND pwd=?;', user)
 	usr_login = cursor.fetchone()
 	
-	return usr_login
+	if usr_login == None:
+		print('You fucked up!')
+	else:
+		return usr_login[0]
 
 
-
-def login_screen():
-	print("Welcome! Please (L)ogin or (E)xit.")
-	action = input("Action: ")
-	action = action.upper()
-	if action == 'L':
-		#os.system('cls' if os.name=='nt' else 'clear')
-		print("Log In: ")
-		uid = input("User ID: ")
-		password = getpass.getpass('Password: ')
-		user = login(uid,password)
-		if user != None:
-			user_menu(user)
+def login_screen():	
+	while True:
+		print("Welcome! Please (L)ogin or (E)xit.")
+		action = input("Action: ")
+		action = action.upper()
+		if action == 'L':
+			os.system('cls' if os.name=='nt' else 'clear')
+			print("Please Log In")
+			uid = input("User ID: ")
+			password = getpass.getpass('Password: ')
+			user = login(uid,password)
+			if user == 'a':
+				agent_menu(uid,password)
+			elif user == 'o':
+				officer_menu(uid,password)
+		elif action == 'E':
+			break
 		else:
-			print("Invalid email or password!")
+			print('Unknown action :(')
+			os.system('cls' if os.name=='nt' else 'clear')
+	return
+		
+
+def agent_menu(uid, password):
+	while True:
+		print("Welcome "+ uid +" "+ password +"!")
+		print("What would you like to do?")
+		print("1 - Register a birth\n2 - Register a marriage\n3 - Renew a vehicle registration\n4 - Process a bill of sale\n5 - Process a payment\n6 - Get a driver abstract")
+		print("L - Logout")
+		action = input("Action: ")
+		if action == '1':
+			register_birth()
+		elif action == '2':
+			register_marriage()
+		elif action == '3':
+			renew_registration()
+		elif action == '4':
+			process_BOS()
+		elif action == '5':
+			process_payment()
+		elif action == '6':
+			get_driver()
+		elif (action == 'L') or (action == 'l'):
+			print("Good byeeee")
+			time.sleep(2)
+			break
+		else:
+			print("Invalid entry!\n Please try again.")
+			os.system('cls' if os.name=='nt' else 'clear')
+
+def officer_menu(uid,pwd):
+	pass
+
+
+
+def register_birth():
+	pass
+def register_marriage():
+	pass
+def renew_registration():
+	pass
+def process_BOS():
+	pass
+def process_payment():
+	pass
+def get_driver():
+	pass
+
 
 
 def main():
